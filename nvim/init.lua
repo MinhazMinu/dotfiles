eeaf2a8f4ddecc09d639766df3f8db40fff99292
MinhazMinu <minhaz.taher@gmail.com>
2023-08-14 19:46:57 +0600

    init.lua file is created

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+--
+--                            ⢸⣦⡈⠻⣿⣿⣿⣶⣄
+--                            ⢸⣿⣿⣦⡈⠻⣿⣿⣿⣷⣄
+--                      ⣀⣀⣀⣀⣀⣀⣼⣿⣿⣿⣿ ⠈⠻⣿⣿⣿⣷⣄
+--                      ⠈⠻⣿⣿⣿⣿⣿⡿⠿⠛⠁   ⠈⠻⢿⣿⣿⣷⣄
+--
+-- Personal Neovim configuration of minhaz << inspired from <jessarcher & theprime>
+
+require('user.plugins')
+require('user.options')
+require('user.keymaps')
+require('user.misc')
+
+-- vim: nospell
