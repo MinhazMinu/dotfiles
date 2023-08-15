dd879381049432737dd1a2cdcf593b82674f0f46
MinhazMinu <minhaz.taher@gmail.com>
2023-08-15 14:59:34 +0600

    before git intrigration

---
+++ nvim/lua/user/plugins/indent-blankline.lua
@@ -0,0 +1,22 @@
+ local hl_list = {}
+  for i, color in pairs({ '#101d08', '#2a1930', '#0e2d30','#303932', '#181010' }) do
+    local name = 'IndentBlanklineIndent' .. i
+    vim.api.nvim_set_hl(0, name, { fg = color })
+    table.insert(hl_list, name);
+  end
+require('indent_blankline').setup({
+  char_highlight_list = hl_list,
+  filetype_exclude = {
+    'help',
+    'terminal',
+    'dashboard',
+    'packer',
+    'lspinfo',
+    'TelescopePrompt',
+    'TelescopeResults',
+  },
+  buftype_exclude = {
+    'terminal',
+      'NvimTree',
+  },
+})
