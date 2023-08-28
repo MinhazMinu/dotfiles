ddcad70390fbc6a2c304cf88379189995158dcf6
MinhazMinu <minhaz.taher@gmail.com>
2023-08-28 16:41:57 +0600

    snippets added

--- nvim/lua/user/plugins/indent-blankline.lua
+++ nvim/lua/user/plugins/indent-blankline.lua
@@ -1,8 +1,8 @@
- local hl_list = {}
-  for i, color in pairs({ '#101d08', '#2a1930', '#0e2d30','#303932', '#181010' }) do
-    local name = 'IndentBlanklineIndent' .. i
-    vim.api.nvim_set_hl(0, name, { fg = color })
-    table.insert(hl_list, name);
+ local hl_list = {}
+  for i, color in pairs({ '#101d08', '#2a1930', '#0e2d30','#303932', '#181010' }) do
+    local name = 'IndentBlanklineIndent' .. i
+    vim.api.nvim_set_hl(0, name, { fg = color })
+    table.insert(hl_list, name);
   end
 require('indent_blankline').setup({
   char_highlight_list = hl_list,
